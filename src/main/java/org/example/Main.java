461bf8b8645d057788ba8e544813671cab6276d1
Antoine Vigneau <antoine.vigneau@sonarsource.com>
2025-06-10 16:36:10 +0200

    Changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello crazy world!");
+    System.out.println("Hello crazy world! 1");
+    System.out.println("Hello crazy world! 2");
+    System.out.println("Hello crazy world! 3");
   }
 }
