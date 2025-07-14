f86b33eabf64cbd7ca98c61e9e8d9de31cb07126
Tom <tom.vanbraband@sonarsource.com>
2025-07-14 17:13:57 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello crazy world!");
     // what am I doing with my life?
-    
+    // I still don't know
   }
 }
